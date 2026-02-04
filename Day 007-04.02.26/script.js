51af3d2562157c29a1e8694145f3261e28371934
Adeela Azeez <adeelaazeez9@gmail.com>
2026-02-04 10:17:12 +0530

    added new code

--- Day 007-04.02.26/script.js
+++ Day 007-04.02.26/script.js
@@ -1,13 +1,49 @@
+// const printYes=()=>{
+//     console.log("Yes")
+// };
+
+// const printNo=()=>{
+//     console.log("No")
+// };
+
+// const sum=(a,b,cb) =>{
+//     const ans=a+b;
+//     cb(ans);
+// };
+// sum(10, 5, printYes);
+
+// console.log("----------------------------START----------------------------");
+// const printYes=()=>{
+//     console.log("Yes")
+// };
+// const printNo=()=>{
+//     console.log("No")
+// };
+
+// const sum=(a,b,cb) =>{
+//     console.log("-------------------Inside SUM--------------------");
+//     const ans=a+b;
+//     window.setTimeout(printNo,1000);
+//     cb(ans);
+//     console.log("-------------SUM is about to complete--------------------");
+// };
+// sum(10, 5, printYes);
+// console.log("----------------------------END----------------------------");
+
+console.log("----------------------------START----------------------------");
 const printYes=()=>{
     console.log("Yes")
 };
-
 const printNo=()=>{
     console.log("No")
 };
 
 const sum=(a,b,cb) =>{
+    console.log("-------------------Inside SUM--------------------");
     const ans=a+b;
+    window.setTimeout(printNo,);
     cb(ans);
+    console.log("-------------SUM is about to complete--------------------");
 };
-console.log(10, 5, printYes);
+sum(10, 5, printYes);
+console.log("----------------------------END----------------------------");
