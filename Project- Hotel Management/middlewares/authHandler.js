9863a9fa2c416dd47146506505e983b5c797e80d
Adeela Azeez <adeelaazeez9@gmail.com>
2026-04-21 14:43:37 +0530

    Enhance validateUser middleware to include token verification and error handling

--- Project- Hotel Management/middlewares/authHandler.js
+++ Project- Hotel Management/middlewares/authHandler.js
@@ -1,8 +1,35 @@
+const jwt=require("jsonwebtokenn")
 const validateUser = (req, res, next) => {
     // authorization cookie?
     // token ? valid ?
     // req.user = decoded data
     // next()
+    try {
+        // 1. Get token from cookies
+        const token = req.cookies?.authorization;
+        if (!token)
+        {
+            return res.status(401).json({
+                success: false,
+                message: "Unauthorized: No token provided"
+            });
+        }
+        // 2. Verify token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        // 3. Attach user data to request
+        req.user = decoded;
+
+        // 4. Move to next middleware/controller
+        next();
+    }
+    catch (err)
+    {
+        return res.status(401).json({
+            success: false,
+            message: "Unauthorized: No token provided"
+        });
+    }
 };
 
 module.exports = { validateUser };
